bd0b9eb99ca617c0f1b6dfbe25b288cf5f1b4cdb
nthapa000 <nishantthapa0000@gmail.com>
2024-05-21 09:10:27 +0530

    #3 star-rating

--- reactProjects/miniprojects/src/App.js
+++ reactProjects/miniprojects/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 // import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
       {/* <Accordian/> */}
 
       {/* Random Color */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
+
+      {/* Star rating */}
+      <StarRating noOfStars={10}/>
     </div>
   );
 }
