2c5508ab30d68695ab7072744270251541963e1b
nthapa000 <nishantthapa0000@gmail.com>
2024-05-21 08:48:38 +0530

    #2 RandomColorGenerator

--- reactProjects/miniprojects/src/App.js
+++ reactProjects/miniprojects/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-import Accordian from './components/accordian';
+// import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/*  Accordian component*/}
-      <Accordian/>
+      {/* <Accordian/> */}
+
+      {/* Random Color */}
+      <RandomColor/>
     </div>
   );
 }
